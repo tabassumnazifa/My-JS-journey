3aa3b23e1e3316e035a40d1cd91efcb114299865
Nazifa Tabassum <132453499+tabassumnazifa@users.noreply.github.com>
2024-09-12 10:47:22 +0600

    Add files via upload

---
+++ concatenate.js
@@ -0,0 +1,9 @@
+document.write("Nazifa" + "Tabassum<br>"); //string concatenate
+var firstname="Nazifa";
+var lastname="Tabassum";
+var fullname=firstname+lastname;
+document.write("My name is "+fullname+"<br>");//string concatenate
+
+var num1=20;
+var num2=50;
+document.write("Number one is "+num1+" number two is "+num2);//number concatenate
